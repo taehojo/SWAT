4d8c506a206a6afebf9da558d89f670355942526
Taeho Jo <taehjo@gmail.com>
2023-07-05 23:28:33 -0400

    no message

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 import argparse
+import pandas as pd
 from scripts.utils import load_data, impute_data, preprocess_data
 from scripts.classifier import train_and_test
 from scripts.visualization import plot_accuracies, plot_feature_importances
 from scripts.api import get_snp_details
+import os
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_accuracies(window_indices, accuracies, filename):
+    plt.plot(window_indices, accuracies, marker='o')
+    plt.xlabel('Window start index')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy over window indices')
+    plt.grid()
+    plt.savefig(filename)
+    plt.close()
+
+def plot_feature_importances(feature_importances, feature_names, filename):
+    plt.bar(range(len(feature_importances)), feature_importances, width=1.5, tick_label=feature_names)
+    plt.xlabel('Feature')
+    plt.ylabel('Importance')
+    plt.title('Feature importances')
+    plt.xticks(np.arange(0, len(feature_importances), 1000), np.arange(0, len(feature_importances), 1000))
+    plt.savefig(filename)
+    plt.close()
 
 def main(file, window_size, imputation_method):
+    # Create a results directory if it doesn't exist
+    if not os.path.exists('results'):
+        os.makedirs('results')
+
+    # Generate a timestamp
+    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+
     # Load and impute data
     data = load_data(file)
     data_imputed = impute_data(data, imputation_method)
@@ -16,18 +46,29 @@ def main(file, window_size, imputation_method):
     clf, accuracies, window_indices = train_and_test(features, labels, window_size)
     
     # Visualize results
-    plot_accuracies(window_indices, accuracies)
-    feature_importances, feature_indices, feature_names = clf.feature_importances_, features.columns[window_indices], features.columns
+    plot_accuracies(window_indices, accuracies, f'results/accuracies_{timestamp}.png')
 
+    # Save accuracy results to CSV
+    accuracy_df = pd.DataFrame(list(zip(window_indices, accuracies)), columns=['window_index', 'accuracy'])
+    accuracy_df.to_csv(f'results/accuracy_results_{timestamp}.csv', index=False)
+      
+    # Derive the feature indices (considering window size) and get the corresponding feature names
+    feature_indices = [idx for window_start in window_indices for idx in range(window_start, min(window_start+window_size, len(features.columns)))]
+    feature_names = features.columns.tolist()
+    
+    # Get the feature importances from the classifier
+    feature_importances = clf.feature_importances_
+    
     # Get SNP details
-    top_20_features = [(feature_names[i], feature_importances[i]) for i in feature_importances.argsort()[-20:][::-1]]
+    top_20_indices = feature_importances.argsort()[-20:][::-1]
+    top_20_features = [(feature_names[i], feature_importances[i]) for i in top_20_indices]
     enriched_top_20_features = get_snp_details(top_20_features)
 
     # Save enriched features to CSV
-    pd.DataFrame(enriched_top_20_features).to_csv('top_20_features.csv', index=False)
+    pd.DataFrame(enriched_top_20_features).to_csv(f'results/top_20_features_{timestamp}.csv', index=False)
 
     # Plot feature importances
-    plot_feature_importances(feature_importances, feature_indices, feature_names)
+    plot_feature_importances(feature_importances, feature_names, f'results/feature_importances_{timestamp}.png')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Perform SNP Analysis')
--- scripts/classifier.py
+++ scripts/classifier.py
@@ -1,11 +1,28 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from joblib import Parallel, delayed
+import numpy as np
 
-def train_and_test(features, labels, test_size=0.2, random_state=42, n_jobs=-1):
-    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=test_size, random_state=random_state)
-    clf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=random_state, n_jobs=n_jobs)
+def train_and_test(features, labels, window_size):
+    def train_test_window(start):
+        if start + window_size > len(features.columns):
+            window_features = features.iloc[:, start:]
+        else:
+            window_features = features.iloc[:, start:start+window_size]
+        train_features, test_features, train_labels, test_labels = train_test_split(window_features, labels, test_size=0.2, random_state=42)
+        clf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=42)
+        clf.fit(train_features, train_labels)
+        pred_labels = clf.predict(test_features)
+        accuracy = accuracy_score(test_labels, pred_labels)
+        return start+1, accuracy
+
+    results = Parallel(n_jobs=-1)(delayed(train_test_window)(i) for i in range(0, len(features.columns), window_size))
+    window_indices, accuracies = zip(*results)
+
+    # Train a classifier on the full set of features
+    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.2, random_state=42)
+    clf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=42)
     clf.fit(train_features, train_labels)
-    pred_labels = clf.predict(test_features)
-    return clf, accuracy_score(test_labels, pred_labels)
 
+    return clf, accuracies, window_indices
