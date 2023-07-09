7a03eb1d4ce28803dc7fd5d8c835c819cf2bb632
Taeho Jo <taehjo@gmail.com>
2023-07-08 21:13:21 -0400

    no message

--- scripts/classifier.py
+++ scripts/classifier.py
@@ -4,7 +4,7 @@ from sklearn.metrics import accuracy_score
 from joblib import Parallel, delayed
 import numpy as np
 
-def train_and_test_windows(features, labels, window_size):
+def train_and_test_windows(features, labels, window_size, num_parallel_jobs):  
     def train_test_window(start):
         if start + window_size > len(features.columns):
             window_features = features.iloc[:, start:]
@@ -17,11 +17,11 @@ def train_and_test_windows(features, labels, window_size):
         accuracy = accuracy_score(test_labels, pred_labels)
         return start+1, accuracy
 
-    results = Parallel(n_jobs=-1)(delayed(train_test_window)(i) for i in range(0, len(features.columns), window_size))
+    results = Parallel(n_jobs=num_parallel_jobs)(delayed(train_test_window)(i) for i in range(0, len(features.columns), window_size))
     window_indices, accuracies = zip(*results)
 
     return accuracies, window_indices
-
+    
 def train_and_test_selected_features(features, labels):
     train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.2, random_state=42)
     clf = RandomForestClassifier(n_estimators=100, random_state=42)
